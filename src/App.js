efa3b4af588907038797f93d1a5d30a625b67e2e
Shivani Sharma <shivani1406sharma@gmail.com>
2022-04-04 02:12:02 -0400

    added login form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import logo from './logo.svg';
 import './App.css';
 
 
 function App() {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
       projectID="9aacb93e-d46a-4391-b40b-cdca47a8ce04"
-      userName="shivani"
-      userSecret="Chat123"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
       />
       
   );
